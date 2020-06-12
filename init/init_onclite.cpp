bf58156a74cfa76773c9695ee3fb9bd56724893a
Jyotiraditya <jyotiraditya@aospa.co>
2020-06-12 00:09:10 -0400

    onclite: init: Add Chinese variants of Redmi 7 to list
    M1810F6LE : Redmi 7 China
    M1810F6LT : Redmi 7 China Mobile
    M1810F6LC : Redmi 7 China Unicom / China Telecom
    
    Signed-off-by: Hadad <repo-sync@outlook.com>
    Change-Id: I61cf10c43b24f23a9a13cc8019610276096a10e2

--- init/init_onclite.cpp
+++ init/init_onclite.cpp
@@ -48,7 +48,8 @@ void load_props(string device, string model) {
 void vendor_load_properties() {
     string boot_cert = android::base::GetProperty("ro.boot.product.cert", "");
 
-    if (boot_cert == "M1810F6LG" || boot_cert == "M1810F6LH" || boot_cert == "M1810F6LI")
+    if (boot_cert == "M1810F6LG" || boot_cert == "M1810F6LH" || boot_cert == "M1810F6LI"
+            || boot_cert == "M1810F6LE" || boot_cert == "M1810F6LT" || boot_cert == "M1810F6LC")
         load_props("onclite", "Redmi 7");
     else
         load_props("onc", "Redmi Y3");
