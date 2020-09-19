57f58bf25014a2869925e091caa09d85bb8937b9
Pig <pig.priv@gmail.com>
2020-09-19 08:57:38 +0800

    audio: Drop LOCAL_COPY_HEADERS usage
    LOCAL_COPY_HEADERS is deprecated, so remove all its usages
    to avoid build warnings.
    
    Change-Id: I4c52508e9109c5f448653fd5613a8991d28bbb2e

--- qahw/Android.mk
+++ qahw/Android.mk
@@ -4,16 +4,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-libqahw-inc := $(LOCAL_PATH)/inc
-
 LOCAL_USE_VNDK := true
 LOCAL_MODULE := libqahwwrapper
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES   := $(libqahw-inc)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
 LOCAL_C_INCLUDES   += $(TARGET_OUT_HEADERS)/common/inc
-
-LOCAL_HEADER_LIBRARIES := \
-    libutils_headers \
 
 LOCAL_SRC_FILES := \
     src/qahw.c \
@@ -27,13 +22,16 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_CFLAGS += -Wall -Werror
 
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw/inc
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
-
 LOCAL_PRELINK_MODULE    := false
 LOCAL_VENDOR_MODULE     := true
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(BUILD_COPY_HEADERS)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqahw_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
 endif
