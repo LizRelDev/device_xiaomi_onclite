LineageOS/android_device_xiaomi_onclite

onclite: Show correct device and model name as per boot cert.
Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>
Change-Id: Ie2a83bf0e2168aaebe1427edb8bd8f8ee4490e56

BoardConfig.mk
@@ -160,6 +160,10 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_onc
+TARGET_RECOVERY_DEVICE_MODULES := libinit_onc
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 

LizRelDev/device_xiaomi_onclite

Signed-off-by: Hadad <repo-sync@outlook.com>
Change-Id: I55646ff4b7c63bf7b54b53e7c2ba5d478de7d2a3

BoardConfig.mk
@@ -162,6 +162,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_onclite
+TARGET_RECOVERY_DEVICE_MODULES := libinit_onclite
+
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864