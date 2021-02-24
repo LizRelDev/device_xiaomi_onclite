660bec1ed1692aaf733bc3aa7488b0a7de55e707
Quallenauge <Hamsi2k@freenet.de>
2021-02-24 19:40:28 +0100

    onclite: Exclude CarrierConfig overlays from RRO's
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Signed-off-by: Hadad <repo-sync@outlook.com>
    Change-Id: I84e21149158a68cc007789afee0509713f90d651

--- device.mk
+++ device.mk
@@ -24,6 +24,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
