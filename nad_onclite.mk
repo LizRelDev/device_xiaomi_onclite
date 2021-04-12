ccec5c7a716a3d1fa022ba3fc9b28181699611b7
Khalif766 <ekaristi699@gmail.com>
2021-04-13 05:06:23 +0700

    Onclite: Update redfin fp to april
    Signed-off-by: Khalif766 <ekaristi699@gmail.com>

--- nad_onclite.mk
+++ nad_onclite.mk
@@ -30,8 +30,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="onc" \
     TARGET_DEVICE="onc" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
