d294849d8f2948a0af96bceb5eaadb96ecf6bdae
Hadad <repo-sync@outlook.com>
2021-05-24 14:13:36 +0700

    onclite: import prebuilt thermal.msm8953.so from MIUI V12.0.1.0 QFLCNXM.
    * onc-user 10 QKQ1.191008.001 V12.0.1.0.QFLCNXM release-keys
    
    - also reverts e409896 changes related thermal 1.0 hal.
    
    Change-Id: Ic8d54e6fd47ab9da4c86b034135816c0459268a3

--- device.mk
+++ device.mk
@@ -455,6 +455,11 @@ PRODUCT_PACKAGES += \
     camera.msm8953_shim \
     libshim_dpmframework
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
