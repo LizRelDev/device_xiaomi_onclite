233fdce350edc5e6ee5c2df65484216f159e67b7
Hadad <repo-sync@outlook.com>
2021-05-27 00:00:00 +0700

    onclite: build vendor variant of vendor.qti.hardware.camera.device@1.0.
    Change-Id: Ia1ce82eda226ce0be28beec97521b2631b64b84c

--- device.mk
+++ device.mk
@@ -164,6 +164,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libgui_vendor \
     libdng_sdk.vendor \
     libstdc++.vendor
