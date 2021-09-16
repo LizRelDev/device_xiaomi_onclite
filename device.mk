8b67f3393af90e6fa776303e2c3bc82dcc6c24b7
Hadad <hadad@linuxmail.org>
2021-09-16 20:00:59 +0700

    onclite: update Performance stack from LA.UM.9.6.2.r1-03600-89xx.0.
    - also delete energy-awareness from rootdir since its gone in R changes of Perf stack
    
    Change-Id: I7c5fde180119f0734578e45c2e8ba8a00e6c1617

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     power.qcom \
     vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/resources/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
