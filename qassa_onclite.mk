e44beb0eb1065fc44fd7be1a1e27a93a2993578c
LizRelDev <dimasmiftah093@gmail.com>
2023-05-02 17:40:49 +0000

    fp: switch to marlin QP1A.191005.007.A3

--- qassa_onclite.mk
+++ qassa_onclite.mk
@@ -33,9 +33,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="onc" \
     TARGET_DEVICE="onc" \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A3 5972272 release-key"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
